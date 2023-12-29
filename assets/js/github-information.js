539a919daff7c19cff8842c1ba5c81dbe4f1b8c0
Joseph O'Connell <LVL27@outlook.ie>
2023-12-29 14:58:02 +0000

    Bugs fixes.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -48,7 +48,7 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..." />
+            <img src="assets/images/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
